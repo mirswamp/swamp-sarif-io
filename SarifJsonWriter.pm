6971fc8d33d0819d2feb5cd99cac6678e74cbd7f
Yuan Zhe Bugh <bugh@wisc.edu>
2018-09-21 14:14:22 -0500

    Added licenses

--- SarifJsonWriter.pm
+++ SarifJsonWriter.pm
@@ -1,5 +1,22 @@
 #!/usr/bin/perl -w
 
+# https://github.com/mirswamp/swamp-sarif-io
+# SWAMP: https://continuousassurance.org
+#
+# Copyright 2018 Yuan Zhe Bugh, James A. Kupsch
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Lincense is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and 
+# limitations under the License.
+
 package SarifJsonWriter;
 use strict;
 use JSON::Streaming::Writer;
